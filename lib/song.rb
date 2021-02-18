a2f67d76809e1dd77bbf29d3e249c0ce91830c39
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-18 01:36:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,14 +39,14 @@ def self.genre_count
 end
  
  def self.artist_count
-    genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
+    artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
     else 
-      genre_count[genre] = 1
+      artist_count[artist] = 1
     end
   end
-  genre_count
+  artist_count
 end
 
